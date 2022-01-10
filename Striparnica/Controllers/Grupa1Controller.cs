b60cdd3352dbd2399edea2eda9f97de30cc5eb8f
shadzijusu1 <shadzijusu1@etf.unsa.ba>
2022-01-10 20:52:10 +0100

    Add project files.

---
+++ Striparnica/Controllers/Grupa1Controller.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Striparnica.Models;
+using System.Collections.Generic;
+
+namespace Striparnica.Controllers
+{
+    public class Grupa1Controller : Controller
+    {
+        static List<Strip> stripovi = new List<Strip>();
+
+        public Grupa1Controller()
+        {
+        }
+
+        // GET: Grupa1
+        public IActionResult Index()
+        {
+            return View(stripovi);
+        }
+
+        // GET: Grupa1/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Grupa1/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("EdicijaStripa,NazivStripa,DatumDospjeća,OpisStripa,Ocjena")] Strip strip)
+        {
+            if (ModelState.IsValid)
+            {
+                if (stripovi.Count < 1)
+                    strip.ID = 1;
+                else
+                    strip.ID = stripovi[stripovi.Count - 1].ID + 1;
+
+                stripovi.Add(strip);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(strip);
+        }
+    }
+}
---
+++ Striparnica/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Striparnica.Models;
+
+namespace Striparnica.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+          : base(options)
+        {
+        }
+
+        public Microsoft.EntityFrameworkCore.DbSet<Strip> Strip { get; set; }
+
+
+    }
+}
